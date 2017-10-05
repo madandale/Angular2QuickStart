821dff55de95a4bf927b2c12315d22768be5426c
madan <madan.dale@gmail.com>
2017-10-05 08:32:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1 class="title">Angular Router</h1>
+    <nav>
+      <a routerLink="/home" routerLinkActive="active">Crisis Center</a>
+      <a routerLink="/about" routerLinkActive="active">Heroes</a>
+
+    </nav>
+    <router-outlet></router-outlet>
+  `
+
 })
 export class AppComponent {
-  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './conents/login/login.component';
+
+import {Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: LoginComponent },
+  { path: 'about',        component: LoginComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
